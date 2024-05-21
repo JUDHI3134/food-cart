49994d6ca5d21c13643a2de978d42aee18bb896d
Umesh604 <umesh604@gmail.com>
2024-05-21 21:09:04 +0530

    add to cart frontend part with backend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -13,11 +13,17 @@ const [token,setToken] = useState("");
 
 const [food_list,setFoodList] = useState([])
 
-const addToCart = (itemId) =>{
+//add to cart
+
+const addToCart = async (itemId) =>{
     if(!cartItems[itemId]){
         setCartItems((prev) => ({...prev,[itemId]:1}))
     }else{
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId]+1}))
+    }
+
+    if(token){
+        await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
     }
 }
 
