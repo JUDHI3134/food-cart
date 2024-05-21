97ae9e0d2cb0e4fa9feeef58e187619b908a6b2b
Umesh604 <umesh604@gmail.com>
2024-05-21 10:37:13 +0530

    complete remove food part in admin panel

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -3,15 +3,13 @@ import './List.css'
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({url}) => {
 
-  const url = "http://localhost:4000";
   const [list,setList] = useState([]);
 
   const fetchList = async () =>{
     const response = await axios.get(`${url}/api/food/list`);
 
-    console.log(response.data)
     if(response.data.success){
       setList(response.data.data)
     }else{
@@ -22,6 +20,16 @@ const List = () => {
   useEffect(()=>{
     fetchList();
   },[])
+
+  const removeFood = async (foodId) =>{
+      const response = await axios.post(`${url}/api/food/remove`,{id:foodId});
+      await fetchList();
+      if(response.data.success){
+        toast.success(response.data.message)
+      }else{
+        toast.error("Error")
+      }
+  }
 
   return (
     <div className='list add flex-col'>
@@ -41,7 +49,7 @@ const List = () => {
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>₹{item.price}</p>
-              <p className='cursor'>X</p>
+              <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
             </div>
           )
         })}
