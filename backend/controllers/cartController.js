c9136506c4eab385c4fce7d5569b330d51ed39f2
Umesh604 <umesh604@gmail.com>
2024-05-21 19:44:42 +0530

    setup route middleware and controller for cart page

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,22 @@
+import userModel from "../models/userModel.js";
+
+//add item to user cart
+
+const addToCart = async (req,res) =>{
+
+}
+
+
+//remove item from user cart
+
+const removeFormCart = async (req,res) =>{
+
+}
+
+// fetch cart data
+
+const getCart = async (req,res) =>{
+
+}
+
+export {addToCart,removeFormCart,getCart}
